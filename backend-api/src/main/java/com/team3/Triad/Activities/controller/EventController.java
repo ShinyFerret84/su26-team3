62f31fecb40cd8a331015df0bfe157b0af7fd060
Melinda Garren <mgarren84@gmail.com>
2026-07-07 21:23:25 -0400

    Started Event API

---
+++ backend-api/src/main/java/com/team3/Triad/Activities/controller/EventController.java
@@ -0,0 +1,5 @@
+package com.team3.Triad.Activities.controller;
+
+public class EventController {
+    
+}
---
+++ backend-api/src/main/java/com/team3/Triad/Activities/entity/Event.java
@@ -0,0 +1,75 @@
+package com.team3.Triad.Activities.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "events")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String eventName;
+
+    @Column(nullable = false)
+    private String venueName;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private BigDecimal cost;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @Column(nullable = false)
+    private Integer maxCapacity;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @Column(length = 1000)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(columnDefinition = "TEXT")
+    private String included;
+
+    @Column(nullable = false)
+    private Boolean featured = false;
+}
