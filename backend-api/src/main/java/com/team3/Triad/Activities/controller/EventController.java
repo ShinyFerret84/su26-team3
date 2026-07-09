4240821798172010f590098f957e6c502d4c33ae
Melinda Garren <mgarren84@gmail.com>
2026-07-08 22:20:45 -0400

    Create EventController.java

---
+++ backend-api/src/main/java/com/team3/Triad/Activities/controller/EventController.java
@@ -0,0 +1,26 @@
+package com.team3.Triad.Activities.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team3.Triad.Activities.entity.Event;
+import com.team3.Triad.Activities.service.EventService;
+
+@RestController
+@RequestMapping("/api/events")
+@CrossOrigin(origins = "*")
+public class EventController {
+    private final EventService eventService;
+}
