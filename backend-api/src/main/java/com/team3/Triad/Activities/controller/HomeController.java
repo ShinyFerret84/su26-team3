3e0edac8d19e16dcd3d3c5342966ceb7e7b8d42c
MariaBorjaReynoso <mariab7899@icloud.com>
2026-07-22 13:22:06 -0400

    trying to fix error with events showing

--- backend-api/src/main/java/com/team3/Triad/Activities/controller/HomeController.java
+++ backend-api/src/main/java/com/team3/Triad/Activities/controller/HomeController.java
@@ -1,15 +1,27 @@
 package com.team3.Triad.Activities.controller;
 
+import com.team3.Triad.Activities.entity.Event;
+import com.team3.Triad.Activities.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    // HOME PAGE shows the landing page for the application
+    @Autowired
+    private EventService eventService;
+
+    // HOME PAGE shows the landing page with events
     // URL: http://localhost:8080/
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Model model) {
+        // Get all events to display on home page
+        List<Event> events = eventService.getAllEvents();
+        model.addAttribute("events", events);
         return "customer/index";
     }
 
